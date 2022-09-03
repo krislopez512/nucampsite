e655677706c5b9d8557706528e2211634c8aac7f
wandererhutch <103301402+wandererhutch@users.noreply.github.com>
2022-09-03 09:57:50 -0400

    Contact Form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import Header from './components/Header';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import './App.css';
 import Footer from './components/Footer';
-
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <Header />
-        <CampsitesDirectoryPage />
-        <Footer />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='contact' element={<ContactPage />} />
+                <Route path='about' element={<AboutPage />} />
+                <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
